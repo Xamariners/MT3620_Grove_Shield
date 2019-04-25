10d06572791a507fedaa872aef510a4186324d3b
Ben Ishiyama-Levy <ben@xamariners.com>
2019-04-25 14:16:06 +0800

    xamagon is on

--- Samples/GroveLightSensor/GroveLightSensor/main.c
+++ Samples/GroveLightSensor/GroveLightSensor/main.c
@@ -7,12 +7,13 @@
 // applibs_versions.h defines the API struct versions to use for applibs APIs.
 #include "applibs_versions.h"
 #include <applibs/log.h>
-
 #include "mt3620_rdb.h"
-
 #include "Grove.h"
 #include "Sensors/GroveLightSensor.h"
+#include "Sensors/GroveOledDisplay96x96.h"
 #include "Sensors/GroveAD7992.h"
+#include "HAL/GroveI2C.h"
+#include <stdio.h>
 
 // This C application for the MT3620 Reference Development Board (Azure Sphere)
 // outputs a string every second to Visual Studio's Device Output window
@@ -31,6 +32,21 @@ static void TerminationHandler(int signalNumber)
     terminationRequested = true;
 }
 
+void DisplayReading(float reading, char *prefix, int sleep)
+{
+	char buf[50];
+
+	sprintf(buf, "%f", reading);
+
+	char result[100];
+	strcpy(result, prefix);
+	strcat(result, buf);
+
+	putString(result);
+
+	usleep(sleep);
+}
+
 /// <summary>
 ///     Main entry point for this sample.
 /// </summary>
@@ -46,17 +62,26 @@ int main(int argc, char *argv[])
 
 	// Initialize Grove Shield
 	int i2cFd;
-	GroveShield_Initialize(&i2cFd, 9600);
+	GroveShield_Initialize(&i2cFd, 230400);
+	
+	// INIT OLED	
+	GroveOledDisplay_Init(i2cFd, SH1107G);
 
 	// Initialize Light Sensor
-	void *light = GroveLightSensor_Init(i2cFd, 0);
+	/*void *light = GroveLightSensor_Init(i2cFd, 0);*/
+
+	// Word display
+	clearDisplay();
+	setNormalDisplay();
+	setVerticalMode();
 
     // Main loop
     const struct timespec sleepTime = {1, 0};
     while (!terminationRequested) {
-		float value = GroveLightSensor_Read(light);
-		value = GroveAD7992_ConvertToMillisVolt(value);
-        Log_Debug("Light value %dmV\n", (uint16_t)value);
+		/*float value = GroveLightSensor_Read(light);
+		value = GroveAD7992_ConvertToMillisVolt(value);*/
+		DisplayReading(12.2, "Light value:", 100000);
+        //Log_Debug("Light value %dmV\n", (uint16_t)value);
 
         nanosleep(&sleepTime, NULL);
     }
